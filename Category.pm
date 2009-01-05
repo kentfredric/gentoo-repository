d597957396635fb317cc90e8adc46fea5919948b
Kent Fredric <kentfredric@gmail.com>
2009-01-05 19:37:28 +1300

    Category Toking -> packages

--- Category.pm
+++ Category.pm
@@ -2,29 +2,52 @@ package Gentoo::Category;
 
 use Moose;
 use Moose::Util::TypeConstraints;
+use Gentoo::Util::Iterator;
 
 extends qw( Gentoo::Base );
+
 #use Scalar::Util qw/refaddr/;
 
-subtype 'CategoryAtom' => ( 
+subtype 'CategoryAtom' => (
     as 'Str',
     where { $_ =~ /^[-a-z0-9]+$/ },
-    message { 
-        "$_ is not a valid Category Atom."
+    message {
+        "$_ is not a valid Category Atom.";
     },
 );
 
-has 'repository' => ( 
-    isa => 'Gentoo::Repository',
-    is  => 'rw',
+has 'repository' => (
+    isa      => 'Gentoo::Repository',
+    is       => 'rw',
     required => 1,
 );
 
-has 'category_name' => ( 
-    isa => 'CategoryAtom',
-    is  => 'rw',
+has 'category_name' => (
+    isa      => 'CategoryAtom',
+    is       => 'rw',
     required => 1,
-    coerce => 1,
+    coerce   => 1,
 );
+
+sub packages {
+    my ($self) = shift;
+    my (@dirs);
+    @dirs =
+      glob( $self->{repository}->{directory} . $self->{category_name} . '/*' );
+    @dirs = sort @dirs;
+    my $iterator = 0;
+
+    return Gentoo::Util::Iterator->new(
+        sub {
+            while ( my $d = $dirs[ $iterator++ ] ) {
+                next if !-d $d;
+                $d =~ s{^\Q$self->{'repository'}->{'directory'}\E}{};
+                $d =~ s{^\Q$self->{'category_name'}\E/}{};
+                return $d;
+            }
+            return;
+        }
+    );
+}
 
 1;
--- Repository.pm
+++ Repository.pm
@@ -10,8 +10,8 @@ extends qw( Gentoo::Base );
 subtype 'Directory' => (
     as 'Str',
     where {
-        return if not -e -r -d $_ ;
-        if ( $_ =~ m{\/$} ){
+        return if not -e -r -d $_;
+        if ( $_ =~ m{\/$} ) {
             return 1;
         }
         $@ = 'Directory needs / at end';
@@ -23,31 +23,32 @@ subtype 'Directory' => (
 );
 
 has 'directory' => (
-    isa => 'Directory',
-    is  => 'rw',
+    isa      => 'Directory',
+    is       => 'rw',
     required => 1,
 );
 
 sub categories {
-    my ($self)    = shift;
-    my @dirs      = glob($self->{directory}.'*');
-    @dirs         = sort @dirs;
-    my $iterator  = 0;
-    return Gentoo::Util::Iterator->new(sub{
-        while ( my $d =  $dirs[$iterator++] ){
-            next if $d =~ qr{/(
+    my ($self) = shift;
+    my @dirs = glob( $self->{directory} . '*' );
+    @dirs = sort @dirs;
+    my $iterator = 0;
+    return Gentoo::Util::Iterator->new(
+        sub {
+            while ( my $d = $dirs[ $iterator++ ] ) {
+                next if $d =~ qr{/(
                 distfiles|eclass|licenses|metadata|packages|profiles|scripts
             )$}mx;
-            next if !-d $d;
-            $d =~ s{^\Q$self->{'directory'}\E}{};
-            return Gentoo::Category->new( 
-                repository      => $self, 
-                category_name   => $d,  
-            );         
+                next if !-d $d;
+                $d =~ s{^\Q$self->{'directory'}\E}{};
+                return Gentoo::Category->new(
+                    repository    => $self,
+                    category_name => $d,
+                );
+            }
+            return;
         }
-        return;
-    });
+    );
 }
- 
 
 1;
